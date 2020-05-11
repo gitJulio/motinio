c8b55cb0a8256811f5d838870333ef7f448b2d60
gitjulio <julio.ezequiel@gmail.com>
2020-05-10 18:59:35 -0600

    first commit

---
+++ guia1.php
@@ -0,0 +1,71 @@
+<html>
+    <head>
+        <title>
+            Comentarios
+        </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<link href="css/heroic-features.css" rel="stylesheet">
+    </head>
+    <body>
+          <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">UTH</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Inicio
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+      
+        <br>
+
+        <center>
+        <h1>
+            Carrera : Ingeniería en producción Industrial
+        </h1>
+            <h1>Comentarios</h1>
+        </center>
+
+        
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col col-lg-12">   
+                    <!-- <h5><h5> -->
+                </div>
+                <div class="col col-lg-12">
+                <p>
+                Propósito de la página web es lograr entender el contenido y la información que se necesita para crear una página web que nosotros los estudiantes entendamos la importancia de crear páginas web porque en un futuro nos puede servir para generar y realizar negocios con muchas personas de todo el mundo no solamente de nuestro país sino que en todo el mundo podemos promocionar diferentes líneas de productos con sus diversas marcas. Que relativamente es uno de los más grandes propósitos de las páginas web que son de distribuir y organizar información pertinente y actual de los diferentes rubros en la industria y el comercio tanto nacional como
+                 internacional Realizar compras y ventas en linea sin tener que ir a un supermercado o una farmacia o cualquier tienda de conveniencia
+                </p>
+                <p>
+                Bueno es una maravillosa experiencia de lograr cursar clases en linea es de mucha utilidad y de muchos beneficios positivos lograr cursar clases vía online porque no necesitamos estar dentro de un salón de clases para poder aprender y estudiar sino que únicamente necesitamos de una buena computadora y de un buen servicio de Internet para garantizar un excelente aprendizaje de forma más rápida sin 
+                costo ni movimiento físico de parte de nosotros los estudiantes Es de mucha utilidad las clases en línea porque también enrriquecemos nuestros conocimientos computacionales y hacemos un mejor uso de todas sus herramientas en este caso de la clase de Informática Aplicada y de muchas otras clases de nuestra carrera y de muchas otras carreras más bendiciones a todas y todos saludos cordiales!!!
+                </p>
+                </div>
+            </div>
+
+
+        </div>        
+
+    </body>
+</html>
+
---
+++ guia2.php
@@ -0,0 +1,96 @@
+<html>
+    <head>
+        <title>
+            Guia 2
+        </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<link href="css/heroic-features.css" rel="stylesheet">
+    </head>
+    <body>
+          <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">UTH</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Inicio
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+        <br>
+
+
+          <center>
+          <h1>INFO UTH</h1>
+          </center>
+               
+
+
+        
+        <div class="container">
+            <div class="row justify-content-md-center">
+             
+                <div class="col col-lg-12">
+                <p>
+
+                La UTH es una institución de mucho prestigio y de mayor crecimiento de Honduras. Estamos interesados en que los estudiantes y docentes tengan la oportunidad de enfrentar los desafíos en un mundo globalizado, 
+                con una economía de libre mercado y altamente competitiva.
+
+                En Honduras existen diez campus universitarios de la UTH, ubicados en San Pedro Sula, Choloma, Puerto Cortés, El Progreso, La Ceiba, Santa Bárbara, Siguatepeque, Tegucigalpa, Choluteca y Roatán. Cuenta con un centro asociado para la educación a distancia (On line).
+
+Una de las características principales de la UTH es que su sistema de enseñanza está estandarizado para garantizar la misma calidad en los diferentes campus, capacitando a los docentes y ofreciendo a los estudiantes un futuro profesional prometedor por la enseñanza de alto valor que se les ha inculcado.
+
+La UTH continúa su proceso de internacionalización al suscribir convenios con diferentes universidades de gran prestigio en Centroamérica, América del sur, Norteamérica y Europa; además estableciendo alianzas con organismos internacionales de desarrollo para realizar proyectos en las zonas más vulnerables del país. Esperamos que en su recorrido por nuestro portal, ustedes conozcan y sean parte de la UTH.
+
+
+                </p>
+
+                <h1>
+                Misión
+                </h1>
+                <p>
+                Brindar educación superior de excelencia, mediante la preparación de profesionales con capacidad y liderazgo, que contribuya al desarrollo económico y social de Honduras, dentro del sistema universal de libre empresa.
+                </p>
+
+                <h1>
+                Visión
+                </h1>
+                <p>
+                La UTH se mantendrá a la vanguardia y liderazgo en la educación superior,
+                 apoyando el desarrollo socio económico y social de Honduras.
+                </p>
+                <h1>
+                Responsabilidad
+                </h1>
+                    <p>
+En la UTH nos comprometemos a servir a la sociedad brindando excelencia académica, con tecnología de avanzada y los mejores profesionales académicos, enfocados a la mejora continua, satisfaciendo las necesidades de nuestros estudiantes y de la empresa privada, pública y partes interesadas.
+                    </p>
+                </div>
+            </div>
+
+
+
+        </div>        
+
+    </body>
+</html>
+
