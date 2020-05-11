0e36f4ac726045193668c10ece4129c2d7a55791
gitjulio <julio.ezequiel@gmail.com>
2020-05-10 19:06:08 -0600

    aa

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Edwin Josué Motiño Burgos</title>
+
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <link href="css/heroic-features.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">UTH</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="index.php">Inicio
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <!-- <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Services</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contacto</a>
+          </li> -->
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Jumbotron Header -->
+    <header class="jumbotron my-4">
+      
+      <img src="img/logo.png">
+      <center>
+       <h1 class="display-3">Venta de articulos</h1>
+
+      
+      </center>
+
+      <!-- <a href="#" class="btn btn-primary btn-lg">Call to action!</a> -->
+    </header>
+
+    <!-- Page Features -->
+    <div class="row text-center">
+
+
+    
+      <!---Guia 1--->
+    
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card">
+        <h1>
+            Comentarios
+          </h1>
+          <div class="card-footer">
+            <a href="guia1.php" class="btn btn-primary"  target="_blank">Ir Comentarios</a>
+          </div>
+        </div>
+      </div>
+
+            <!---Guia 1--->
+     
+            <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card">
+          <!-- <img class="card-img-top" src="img/guia2.png" alt=""> -->
+          <h1>
+            UTH
+          </h1>
+          <!-- <div class="card-body">
+            <h4 class="card-title">Card title</h4>
+            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
+          </div> -->
+          <div class="card-footer">
+            <a href="guia2.php" class="btn btn-primary">Ir Info UTH</a>
+          </div>
+        </div>
+      </div>
+
+      
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card">
+        <h1>
+            Link UTH
+          </h1>
+          <div class="card-footer">
+            <a href="https://www.uth.hn/" class="btn btn-primary">Ir UTH</a>
+          </div>
+        </div>
+      </div>
+
+          
+
+
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <!-- <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div> -->
+    <center class="colo">
+     <h1 class="display-3">Edwin Josué Motiño Burgos</h1>
+      <p class="lead">201010820010.</p>
+      <p class="lead">Informática Aplicada.</p>
+  
+    
+    </center>
+      
+    <!-- /.container -
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <!-- <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script> -->
+
+  <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
